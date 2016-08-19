fe2c534aecea9e16fe47f9f8c7b0f5c803815f10
Rixuel <rixuel@hotmail.com>
2016-08-19 13:11:13 -0400

    test

--- js/preloader.js
+++ js/preloader.js
@@ -9,16 +9,23 @@ var previousURL = document.referrer;
 var prevParts = previousURL.split('.');
 var prevsub = prevParts[0];
 var prevdomain = prevParts[1];
-
+/*
 console.log("Previous URL: " + previousURL);
 console.log("Prev Subdomain: " + prevsub);
 console.log("Prev Domain: " + prevdomain);
-
+*/
 var full = window.location.href;
 var parts = full.split('.');
 var sub = parts[0];
 var domain = parts[1];
-
+/*
 console.log("Full: " + full);
 console.log("Subdomain: " + sub);
 console.log("Domain: " + domain);
+*/
+
+if ((prevsub == sub) && (prevdomain == domain)) {
+	console.log("Previous URL and current URL matches!!!");
+} else {
+	console.log("Previous URL and current URL don't matches...");
+}
