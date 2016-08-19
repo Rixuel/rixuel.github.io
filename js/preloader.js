0cc303cbec9eb5e83af4bbfe98821b2d359c10ee
Rixuel <rixuel@hotmail.com>
2016-08-19 13:06:22 -0400

    test

--- js/preloader.js
+++ js/preloader.js
@@ -4,3 +4,21 @@ setTimeout(function () {
 	document.body.style.overflow = 'visible';
 }, 5000);
 document.body.scrollTop = document.documentElement.scrollTop = 0;
+
+var previousURL = document.referrer;
+var prevParts = previousURL.split('.');
+var prevsub = prevParts[0];
+var prevdomain = prevParts[1];
+
+console.log("Previous URL: " + previousURL);
+console.log("Prev Subdomain: " + prevsub);
+console.log("Prev Domain: " + prevdomain);
+
+var full = window.location.href;
+var parts = full.split('.');
+var sub = parts[0];
+var domain = parts[1];
+
+console.log("Full: " + full);
+console.log("Subdomain: " + sub);
+console.log("Domain: " + domain);
