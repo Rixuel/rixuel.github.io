3e138f04f6a572806710c89e96034c24a3f115dd
Rixuel <rixuel@hotmail.com>
2016-08-21 19:18:49 -0400

    optimizing pics 2

--- js/adstuff.js
+++ js/adstuff.js
@@ -4,7 +4,7 @@ function adsbanners(){"use strict";var ads,index;ads=[];
 			"<img src='images/alternalogo.png' border='0'/></a>";
 
 	ads[1]= "<a href='#/'>"+
-			"<img src='images/alternalogo.png' border='0'/></a>";
+			"<img src='images/terra.png' border='0'/></a>";
 
 index=Math.floor(Math.random()*ads.length);
 document.getElementById("adsfooter").innerHTML=ads[index];}
