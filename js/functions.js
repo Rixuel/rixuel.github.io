63700ee7fad75c7d789ff1756bc8af353181a30a
Rixuel <rixuel@hotmail.com>
2016-08-22 21:48:45 -0400

    modif

--- js/functions.js
+++ js/functions.js
@@ -165,7 +165,6 @@ function showModal() {
 	var modal = document.getElementById("myModal");
 	window.onclick = function(event) {
 		if (event.target == modal) {
-			//modal.style.display = "none";
 			closeModal();
 		}
 	}
