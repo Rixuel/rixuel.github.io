0ae92494d7d7f4002da480ff8e4b0fe47fc77cfe
Rixuel <rixuel@hotmail.com>
2018-10-11 22:19:41 -0400

    Changing preload screen time

--- js/preloader.js
+++ js/preloader.js
@@ -14,15 +14,17 @@ if (!((prevsub === sub) && (prevdomain === domain))) {
 	document.getElementById("loadcont").innerHTML = "<img src='img/rixuelbanner.png' id='fadein'>";
 
 	// Hidden scroll for milliseconds
-	document.body.style.overflow = "hidden";
+	//document.body.style.overflow = "hidden";
+	document.body.style.setProperty("overflow", "hidden", "important");
 
 	// After some milliseconds, make stuff visible
 	setTimeout(function () {
 		"use strict";
-		document.body.style.overflow = "visible";
+		//document.body.style.overflow = "visible";
+		document.body.style.setProperty("overflow", "visible", "important");
 		// Add pointer-events: none; to #loader to be able to click on Edge.
 		document.getElementById("loader").style.pointerEvents = "none";
-	}, 4500);
+	}, 3000);
 
 	document.body.scrollTop = document.documentElement.scrollTop = 0;
 } else {
