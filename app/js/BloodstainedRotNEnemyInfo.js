b4ecbe43abc183c5cd5ff6f6c2a0a09f03eff4a3
Rixuel <rixuel@hotmail.com>
2019-07-07 04:02:07 -0400

    Bloodstained enemy info update page

--- app/js/BloodstainedRotNEnemyInfo.js
+++ app/js/BloodstainedRotNEnemyInfo.js
@@ -1,3 +1,4 @@
+var JSONurl = "../app/json/BloodstainedRotNEnemyInfo.json";
 var searchWord = "";
 var searchBarInput = document.getElementById("searchBar");
 searchBarInput.addEventListener("keyup", function(event) {
@@ -9,47 +10,124 @@ searchBarInput.addEventListener("keyup", function(event) {
 
 function searchInput() {
 	searchWord = document.getElementById("searchBar").value;
-	console.log("searchWord: " + searchWord);
+	//console.log("searchWord: " + searchWord);
 	$('#result').html("");
 }
 
 function getJSON() {
-	console.log("getJSON() Trigger!");
+	var dataResult = "";
 
 	$.ajax({
-		url: "../app/json/BloodstainedRotNEnemyInfo.json",
+		url: JSONurl,
 		type: "get",
 		dataType: 'json',
 		mimeType: "application/json",
 		success: function(data){
-			console.log(data);
-			console.log("Object Length: " + data.length);
 
 			for (var i=0; i<data.length; i++) {
 
 				if (data[i].id == searchWord ||
 					data[i].Name.toLowerCase() == searchWord.toLowerCase() ||
-					data[i].Shard[0].Name.toLowerCase() == searchWord.toLowerCase()){
-					// Search giving unique result
-					$('#result').append(data[i].id + "<br>");
-					$('#result').append(data[i].Name + "<br>");
-					$('#result').append("Shard: " + data[i].Shard[0].Name + "<br>");
-					$('#result').append("Shard rate: " + data[i].Shard[0].Rate + "% <br>");
-					$('#result').append("Shard type: " + data[i].Shard[0].Type + "<br>");
-					$('#result').append(data[i].Description + "<hr>");
+					data[i].Shard[0].Name.toLowerCase() == searchWord.toLowerCase() ||
+					data[i].Shard[0].Type.toLowerCase() == searchWord.toLowerCase()){
+					// Search one or multiple results
+					dataResult += "<h1><b>" + data[i].Name + " (</b>ID: " + data[i].id + ")</h1>";
+					dataResult += "LV: " + data[i].Stats[0].LV + " | EXP: " + data[i].Stats[0].EXP + " | HP: " + data[i].Stats[0].HP + "<br>";
 
-					console.log("Drop count: " + data[i].Drop.length);
-				} else if (data[i].Shard[0].Type.toLowerCase() == searchWord.toLowerCase()) {
-					// Search giving multiple results
-					$('#result').append(data[i].id + "<br>");
-					$('#result').append(data[i].Name + "<br>");
-					$('#result').append("Shard: " + data[i].Shard[0].Name + "<br>");
-					$('#result').append("Shard rate: " + data[i].Shard[0].Rate + "% <br>");
-					$('#result').append("Shard type: " + data[i].Shard[0].Type + "<br>");
-					$('#result').append(data[i].Description + "<hr>");
+					dataResult += "<div class='columns'>";
+						dataResult += "<div class='column is-half'>";
+						dataResult += "<br><b>Resist: </b><br>";
+						dataResult += "- Slash: " + data[i].Resist[0].Slash + "<br>";
+						dataResult += "- Thrust: " + data[i].Resist[0].Thrust + "<br>";
+						dataResult += "- Strike: " + data[i].Resist[0].Strike + "<br>";
+						dataResult += "- Fire: " + data[i].Resist[0].Fire + "<br>";
+						dataResult += "- Ice: " + data[i].Resist[0].Ice + "<br>";
+						dataResult += "- Thunder: " + data[i].Resist[0].Thunder + "<br>";
+						dataResult += "- Light: " + data[i].Resist[0].Light + "<br>";
+						dataResult += "- Darkness: " + data[i].Resist[0].Darkness + "<br>";
+						dataResult += "- Poison: " + data[i].Resist[0].Poison + "<br>";
+						dataResult += "- Curses: " + data[i].Resist[0].Curses + "<br>";
+						dataResult += "- Petrification: " + data[i].Resist[0].Petrification + "<br>";
+						dataResult += "</div>";
+
+						dataResult += "<div class='column'>";
+
+
+
+						dataResult += "<br><b>" + data[i].Shard[0].Type + " Shard: </b><br>";
+						dataResult += "- " + data[i].Shard[0].Name + " (" + data[i].Shard[0].Rate + "%)<br>";
+						dataResult += "<br><b>Drop: </b><br>";
+						for (var ii=0; ii<data[i].Drop.length; ii++) {
+							dataResult += "- " + data[i].Drop[ii].Item + " (" + data[i].Drop[ii].Rate + "%)" + "<br>";
+						}
+						dataResult += "<br><b>Area: </b><br>";
+						for (var ii=0; ii<data[i].Area.length; ii++) {
+							dataResult += "- " + data[i].Area[ii] + "<br>";
+						}
+						dataResult += "</div>";
+					dataResult += "</div>";
+					dataResult += data[i].Description + "<hr>";
+
+					document.getElementById("result").innerHTML = dataResult;
 				} else if (searchWord == "") {
-					$('#result').html("Enter something");
+					document.getElementById("result").innerHTML = "Enter something";
 				}
+
+			}
+
+		}
+	});
+}
+
+function showAll() {
+	var dataResult = "";
+
+	$.ajax({
+		url: JSONurl,
+		type: "get",
+		dataType: 'json',
+		mimeType: "application/json",
+		success: function(data){
+
+			for (var i=0; i<data.length; i++) {
+				dataResult += "<h1><b>" + data[i].Name + " (</b>ID: " + data[i].id + ")</h1>";
+				dataResult += "LV: " + data[i].Stats[0].LV + " | EXP: " + data[i].Stats[0].EXP + " | HP: " + data[i].Stats[0].HP + "<br>";
+
+				dataResult += "<div class='columns'>";
+					dataResult += "<div class='column is-half'>";
+					dataResult += "<br><b>Resist: </b><br>";
+					dataResult += "- Slash: " + data[i].Resist[0].Slash + "<br>";
+					dataResult += "- Thrust: " + data[i].Resist[0].Thrust + "<br>";
+					dataResult += "- Strike: " + data[i].Resist[0].Strike + "<br>";
+					dataResult += "- Fire: " + data[i].Resist[0].Fire + "<br>";
+					dataResult += "- Ice: " + data[i].Resist[0].Ice + "<br>";
+					dataResult += "- Thunder: " + data[i].Resist[0].Thunder + "<br>";
+					dataResult += "- Light: " + data[i].Resist[0].Light + "<br>";
+					dataResult += "- Darkness: " + data[i].Resist[0].Darkness + "<br>";
+					dataResult += "- Poison: " + data[i].Resist[0].Poison + "<br>";
+					dataResult += "- Curses: " + data[i].Resist[0].Curses + "<br>";
+					dataResult += "- Petrification: " + data[i].Resist[0].Petrification + "<br>";
+					dataResult += "</div>";
+
+					dataResult += "<div class='column'>";
+
+
+
+					dataResult += "<br><b>" + data[i].Shard[0].Type + " Shard: </b><br>";
+					dataResult += "- " + data[i].Shard[0].Name + " (" + data[i].Shard[0].Rate + "%)<br>";
+					dataResult += "<br><b>Drop: </b><br>";
+					for (var ii=0; ii<data[i].Drop.length; ii++) {
+						dataResult += "- " + data[i].Drop[ii].Item + " (" + data[i].Drop[ii].Rate + "%)" + "<br>";
+					}
+					dataResult += "<br><b>Area: </b><br>";
+					for (var ii=0; ii<data[i].Area.length; ii++) {
+						dataResult += "- " + data[i].Area[ii] + "<br>";
+					}
+					dataResult += "</div>";
+				dataResult += "</div>";
+				dataResult += data[i].Description + "<hr>";
+
+				document.getElementById("result").innerHTML = dataResult;
 			}
 
 		}
