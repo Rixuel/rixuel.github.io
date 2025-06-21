52a4362562e1b78b127c932907d223f770926181
Rixuel <rixuel@hotmail.com>
2025-06-20 21:21:13 -0400

    add more modes for fan username

--- js/random-fantasy-user.js
+++ js/random-fantasy-user.js
@@ -27,7 +27,7 @@ let fantasy = [
     "ze", "zeni", "zeta", "zion", "zu", "zuel"
 ];
 
-let euro = [
+let german = [
     "ak", "al", "alex", "art", "auf", "augs",
     "bar", "be", "bene", "bern", "bert", "bin", "borg", "brun", "burg",
     "caci", "cari", "chim",
@@ -52,7 +52,30 @@ let euro = [
     "zel", "zent", "zenz", "zu"
 ];
 
-let asia = [
+let greek = [
+    "aca", "achai", "aes", "aga", "agn", "al", "alex", "am", "anas", "ander", "andra", "andros", "aphro", "apol", "arca", "arche", "aris", "arte", 
+    "brosia", "brosios",
+    "caeus", "cass", "chrys", "chylus", "cius", "cle", "clid", "co", "cus", "cy",
+    "deme", "demo", "demon", "dion", "disia", "dius", "dora", "doros", "dra",
+    "eas", "ene", "eras", "es", "eu", "euge",
+    "gai", "gios", "gius", 
+    "hele", "hera", "herm", "hiero",
+    "ia", "idas", "ios", "ire", "is", "isi", 
+    "ka", "kalli", "kar", "kass", "kleo", "kos",
+    "laos", "leon", "lia", "lige", "lin", "lon", "ly", "lys", 
+    "mache", "melis", "misia", "mos", "my",
+    "na", "ne", "neo", "nico", "nius", "nymos",
+    "olym", "on", "op", "orphe", "os",
+    "panthe", "pela", "pelios", "pelius", "petos", "phia", "phil", "pho", "phon", "phosa", "phus", "pos", "pus", 
+    "ras", "ron", "roxa", "rus",
+    "sa", "so", "stos", "strate",
+    "tasia", "tasios", "tha", "the", "theo", "thon", "tides", "tocles", "tolius", "ton", "tos", "tria", "trios", "try",
+    "xan", "xen", "xeno",
+    "ysius",
+    "zeno", "zenon"
+];
+
+let japanese = [
     "ai", "aiko", "aki", "ao", "asu", "aya", "ayu",
     "ba", "bu", "bi",
     "chi", "cho",
@@ -75,6 +98,121 @@ let asia = [
     "za", "zue", "zuki", "zuo", "zu"
 ];
 
+let korean = [
+    "an", 
+    "bang", "bok", 
+    "cha", "chang", "cho",
+    "dong",
+    "eul", "eun",
+    "gi", "gu", "gwa", "gye",
+    "ha", "han", "ho", "hun", "hwa", "hye", "hyung",
+    "is",
+    "jae", "je", "ji", "jin", "jo", "jong", "joon", "jun",
+    "ki", "kim", "ky",
+    "lee",
+    "ma", "moon", "mye",
+    "nim",
+    "ok", "on",
+    "park",
+    "ri",
+    "san", "sang", "se", "seo", "shin", "si", "song", "sung",
+    "uk", "ul", "ung",
+    "wo",
+    "ye", "yo", "yong", "young", "yu"
+];
+
+let roman = [
+    "ael", "aem", "aet", "al", "amo", "ana", "aur", "augus", "avi", "avia",
+    "bal", "blan", "blas", "bru",
+    "cae", "cael", "caes", "cai", "cami", "cass", "cat", "cel", "cice", "cilla", "cius", "cimus", "corne", "cretia", "cus",
+    "dela", "dus",
+    "en", "el", "ele",
+    "fab", "faust", "fer", "fla", "flo", "flori",
+    "ia", "iana", "ina", "inus", "ius", "ix",
+    "gai", "gal", "ginius",
+    "lauren", "lina", "linus", "lius", "lo", "long", "luc", "lus",
+    "mar", "max", "maxi", "milla",
+    "ne", "nelia", "nia", "nius", "no", "non", "nume",
+    "oct",
+    "pau", "pon", "pris",
+    "quin",
+    "regu", "rina", "rus",
+    "sar", "saturn", "secun", "ser", "sever",
+    "ta", "tan", "ti","tiber", "tilius", "tinus", "tus",
+    "ro",
+    "us",
+    "val", "valen", "vari", "vergi", "vi", "vindi", "vini", "vius",
+    "xus"
+];
+
+let russian = [
+    "ad", "aga", "ak", "aki", "aleks", "alex", "am", "an", "ana", "anas", "and", "astas",
+    "bo",
+    "chka",
+    "de", "di", "dik", "dimir", "do", "domir", "dora", "diya",
+    "e", "ei", "eka", "ev",
+    "gala", "gali", "ge", "gei", "gej", "gela", "geor", "geya", "gy",
+    "iri", "isa", "isi", "iana", "ina",
+    "je",
+    "katerina", "ken", "ki", "kla", "kov", "krist",
+    "la", "lai", "lan", "lana", "lav", "len", "lena", "leo", "letta", "lery", "lina", "lisa", "lu",
+    "mak", "mar", "mara", "mi", "mir", "milo", "mitri", "minika",
+    "na", "nast", "nat", "nata", "nei", "ni", "nika", "nid", "niko", "nov", "nya",
+    "ok", "ol",
+    "pa", "pat", "pela", "pet", "pla", "pol", "praskov",
+    "ra", "rado", "rai", "rei", "rina", "ris", "riya", "rya",
+    "sana", "ser", "sevas", "sey", "sha", "sili", "siya", "ska", "sla", "slav", "slava", "svet", "sya",
+    "ta", "tai", "tasia", "tasiya", "tek", "terina", "tian", "ton", "tor", "toriya",
+    "ul", "usha", "usti",
+    "va", "vale", "vel", "vera", "vik", "vio", "vlad", "vladi",
+    "ya", "ye", "yu",
+    "za", "zaliya", "zanna", "zar", "zi", "zino", "zla", "zo"
+];
+
+let vietnamese = [
+    "an", "anh",
+    "bao", "bich", "binh",
+    "cam", "chau", "chi", "chim", "cho", "chua", "cuc",
+    "dai", "dao", "dat", "diep", "dep", "dinh", "do", "dong", "duc", "dung", "duong",
+    "hao", "hiep", "ho", "hoa", "hong", "hue", "hung", "huong", "huy",
+    "kiem", "kim", "kin",
+    "lan", "lanh", "linh", "loc",
+    "mai", "meo", "minh", "moi", "mot", 
+    "ngai", "ngo", "ngoc", "ngu", "nguy", "nhung", "nu",
+    "ong", "oc",
+    "pho", "phu", "phuc",
+    "ron",
+    "sang", "set", "si", "sung",
+    "quan", "quang", "quy", "quyen",
+    "tien", "thanh", "thi", "tho", "thu", "thuan", "thuy", "tien", "trai", "trinh", "troi", "truc", "tu", "tuan", "tu", "tuan",
+    "van", "vien", "vinh", "vua", 
+    "xu",
+    "yen", "yet"
+];
+
+let vulgar = [
+    "ass", "arse",
+    "bag", "bastard", "bitch", "bull",
+    "cow", "crap", "creep",
+    "damn", "dirty", "dog", "dork", "dumb",
+    "eat", "er", "est", "evil",
+    "fat", "fart", "fool", "face",
+    "ginger",
+    "hag", "head", "ho",
+    "ier", "iest", "idiot",
+    "jizz",
+    "lard", "lame", "lose", "low",
+    "meat", "moron",
+    "noob", "nub",
+    "pansy", "poor", "prick", "puss",
+    "re",
+    "shame", "shit", "stink", "stupid",
+    "tard", "trash", "turd",
+    "ugly",
+    "vile",
+    "whore", "woop", "wuss"
+];
+
 let selectedMode = [];
 let selectedModeLength = 0;
 let fanUserSelectedMode = "";
@@ -90,15 +228,23 @@ function fanuserExecute() {
         //console.log("fanUserSelectedMode: " + fanUserSelectedMode);
         //console.log("fanUserNameLength: " + fanUserNameLength);
 
-        if (fanUserSelectedMode == "Fantasy") {
-            selectedMode = fantasy;
-            selectedModeLength = fantasy.length;
-        } else if (fanUserSelectedMode == "Euro") {
-            selectedMode = euro;
-            selectedModeLength = euro.length;
-        } else if (fanUserSelectedMode == "Asia") {
-            selectedMode = asia;
-            selectedModeLength = asia.length;
+        const modes = {
+            Fantasy: fantasy,
+            German: german,
+            Greek: greek,
+            Japanese: japanese,
+            Korean: korean,
+            Roman: roman,
+            Russian: russian,
+            Vietnamese: vietnamese,
+            Vulgar: vulgar
+        };
+
+        const selectedArray = modes[fanUserSelectedMode];
+
+        if (selectedArray) {
+            selectedMode = selectedArray;
+            selectedModeLength = selectedArray.length;
         }
         
         let tempString = "";
