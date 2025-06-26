8e4927a141adfa2230b8e657c4f344ccf8959e33
Rixuel <rixuel@hotmail.com>
2025-06-26 16:07:57 -0400

    fix top main char row html when anime title is way toooo long

--- app/anime/js/anime.js
+++ app/anime/js/anime.js
@@ -345,19 +345,21 @@ async function checkTopCharacters(malId) {
                 .sort((a, b) => b.favorites - a.favorites)
                 .slice(0, 10);
             
-            // Generate HTML
+            // Generate Top Main Char row HTML
             const listHTML = top10char.map((char, index) => `
                     <li class="d-flex align-items-center mb-2 custom-top-char-row">
-                        <div class="me-2 text-center" style="width: 40px;">
+                        <div class="me-2 text-center" style="width: 40px; flex-shrink: 0;">
                             <span class="fs-5">${index + 1}</span>
                         </div>
-                        <img src="${char.image}" alt="${char.name}" class="me-2 rounded" style="width: 50px; height: 50px; object-fit: cover;">
-                        <div class="text-start flex-grow-1">
-                            <div><strong class="fs-6">${char.name}</strong> <span class="text-secondary small">(#${char.id})</span></div>
-                            <div class="text-info"><em>${char.animeTitle}</em></div>
+                        <img src="${char.image}" alt="${char.name}" class="me-2 rounded"
+                            style="width: 50px; height: 50px; object-fit: cover; flex-shrink: 0;">
+
+                        <div class="flex-grow-1 text-start" style="min-width: 0;">
+                            <div><strong class="fs-6 text-break me-1">${char.name}</strong><span class="text-secondary small">(#${char.id})</span></div>
+                            <div class="text-info text-break"><em>${char.animeTitle}</em></div>
                             <div class="text-warning small">❤ ${char.favorites.toLocaleString()} favorites</div>
                         </div>
-                    </li>
+                     </li>
                 `)
                 .join('');
             document.getElementById('vaModalCharacters').innerHTML = `
