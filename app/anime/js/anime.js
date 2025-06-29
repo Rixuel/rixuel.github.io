a3cf44694bd1fa7115094e2fe6aa0e14e8eceaf4
Rixuel <rixuel@hotmail.com>
2025-06-28 22:17:25 -0400

    retrying 500 delay

--- app/anime/js/anime.js
+++ app/anime/js/anime.js
@@ -448,7 +448,7 @@ async function getCharacterFavorites(charMalId, retry = 2) {
     //console.log("Characters Info URL: ", `https://api.jikan.moe/v4/characters/${charMalId}`);
     console.log("Getting Character with favorites...");
     try {
-        await delay(1000); // To avoid API rate limit
+        await delay(500); // To avoid API rate limit
         const response = await fetch(`https://api.jikan.moe/v4/characters/${charMalId}`);
         // Check for 429 Too Many Requests
         if (response.status === 429) {
